2dc2cd234345da9e4f9d6e2d9ab9822353b9e791
Alex Tran <tranduclong092@gmail.com>
2024-06-22 16:27:21 +0300

    Feat: style nav elements

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -28,6 +28,31 @@ export const navData = [
 
 export default function Nav() {
   return (
-    <div>Nav</div>
+    <nav
+      className="
+        flex flex-col items-center fixed h-max w-full
+        gap-y-4 bottom-0 mt-auto z-50 top-0
+        xl:justify-center xl:right-[2%] xl:w-16 xl:max-w-md xl:h-screen
+      "
+    >
+      { }
+      <div
+        className="
+          flex w-full items-center justify-between h-[80px]
+          gap-y-10 px-4 py-8 bg-white/10 backdrop-blur-sm text-3xl 
+          xl:flex-col xl:justify-center xl:text-xl xl:rounded-full 
+          xl:px-0 xl:h-max md:px-40 
+        "
+      >
+        {navData.map((link, index) => (
+          <Link
+            key={`${index}_${link.name}`}
+            href={link.path}
+          >
+            <div>{link.icon}</div>
+          </Link>
+        ))}
+      </div>
+    </nav >
   )
 }
