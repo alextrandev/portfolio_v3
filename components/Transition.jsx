ff02837930a847c52395af6a4a3dbb483f9db7bb
Alex Tran <tranduclong092@gmail.com>
2024-07-08 17:37:11 +0300

    Feat: create a page transition div

--- components/Transition.jsx
+++ components/Transition.jsx
@@ -1,5 +1,31 @@
+import { motion } from "framer-motion";
+
+const transitionVariants = {
+  initial: {
+    x: '100%',
+    width: '100%'
+  },
+  animate: {
+    x: '0%',
+    width: '0%'
+  },
+  exit: {
+    x: ['0%', '100%'],
+    width: ['0%', '100%'],
+  }
+};
+
 export default function Transition() {
   return (
-    <div>Transition</div>
+    <>
+      <motion.div
+        className="fixed top-0 right-full w-screen h-screen z-30 bg-[#2e2257]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.2, duration: 0.6, ease: 'easeInOut' }}
+      ></motion.div>
+    </>
   )
 }
