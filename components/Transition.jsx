29718d1c1afd09a4ca68760fe506b700b7325ac8
Alex Tran <tranduclong092@gmail.com>
2024-07-09 13:12:22 +0300

    Feat: add two more transition frames

--- components/Transition.jsx
+++ components/Transition.jsx
@@ -18,6 +18,7 @@ const transitionVariants = {
 export default function Transition() {
   return (
     <>
+      {/* transition frame 1  */}
       <motion.div
         className="fixed top-0 right-full w-screen h-screen z-30 bg-[#2e2257]"
         variants={transitionVariants}
@@ -25,6 +26,26 @@ export default function Transition() {
         animate="animate"
         exit="exit"
         transition={{ delay: 0.2, duration: 0.6, ease: 'easeInOut' }}
+      ></motion.div>
+
+      {/* transition frame 2  */}
+      <motion.div
+        className="fixed top-0 right-full w-screen h-screen z-20 bg-[#3b2d71]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.4, duration: 0.6, ease: 'easeInOut' }}
+      ></motion.div>
+
+      {/* transition frame 3  */}
+      <motion.div
+        className="fixed top-0 right-full w-screen h-screen z-10 bg-[#4b3792]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.6, duration: 0.6, ease: 'easeInOut' }}
       ></motion.div>
     </>
   )
