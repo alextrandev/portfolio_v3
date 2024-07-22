9326983040eb5960f02db946b98ecb43fca97070
Alex Tran <tranduclong092@gmail.com>
2024-07-22 16:50:40 +0300

    Feat: add social icons to header bar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+import Socials from './Socials';
 
 export default function Header() {
   return (
     <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
       <div className="container mx-auto ">
-        <div>
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
           {/* logo */}
           <Link href={'/'} className="flex items-center">
             <Image
@@ -21,6 +22,7 @@ export default function Header() {
             </div>
           </Link>
           {/* Social links */}
+          <Socials />
         </div>
       </div>
     </header>
--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,5 +1,29 @@
+// social link icons
+import Link from 'next/link';
+import { RiInstagramLine, RiGithubFill, RiLinkedinBoxLine, RiDiscordLine } from 'react-icons/ri';
+import { LiaSteam, LiaBehanceSquare } from "react-icons/lia";
+import { TbBrandDrupal } from "react-icons/tb";
+
+// social icons bar
 export default function Socials() {
   return (
-    <div>Socials</div>
+    <div className="flex text-xl gap-5 items-center">
+      <Social url={'https://github.com/alextrandev'} Icon={RiGithubFill} />
+      <Social url={'https://github.com/alextrandev'} Icon={RiLinkedinBoxLine} />
+      <Social url={'https://github.com/alextrandev'} Icon={TbBrandDrupal} />
+      <Social url={'https://github.com/alextrandev'} Icon={RiInstagramLine} />
+      <Social url={'https://github.com/alextrandev'} Icon={LiaBehanceSquare} attr="text-2xl" />
+      <Social url={'https://github.com/alextrandev'} Icon={RiDiscordLine} />
+      <Social url={'https://github.com/alextrandev'} Icon={LiaSteam} />
+    </div>
+  )
+}
+
+// single social icon link to reuse above
+export function Social({ url, Icon, attr }) {
+  return (
+    <Link href={url} target='blank' className={`hover:text-accent transition-all duration-300 ${attr && attr}`}>
+      <Icon />
+    </Link>
   )
 }
