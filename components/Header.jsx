6b1d69d655c20944511864fff4f3733fede458ff
Alex Tran <tranduclong092@gmail.com>
2024-07-22 17:19:50 +0300

    Feat: improve breakpoints styling

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,20 +5,21 @@ import Socials from './Socials';
 export default function Header() {
   return (
     <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
-      <div className="container mx-auto ">
-        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
+      <div className="container mx-auto sm:p-3">
+        <div className="flex flex-row justify-between items-center gap-y-6 py-8 max-sm:flex-col max-sm:gap-0 max-sm:py-5">
           {/* logo */}
-          <Link href={'/'} className="flex items-center">
+          <Link href={'/'} className="flex items-center group">
             <Image
               src={'/logo.svg'}
               width={65}
               height={65}
               alt="Site logo"
               priority={true}
+              className="max-sm:w-10"
             />
-            <div className="hidden xl:flex xl:flex-col">
+            <div className="hidden xl:flex xl:flex-col max-sm:flex max-sm:flex-row max-sm:gap-2 group-hover:text-accent transition-all duration-300">
               <h1 className="h2 text-xl p-0 -mb-2">Alex Tran</h1>
-              <p className="p-0 m-0">portfolio</p>
+              <p className="p-0 m-0 max-sm:text-xl">portfolio</p>
             </div>
           </Link>
           {/* Social links */}
--- components/Socials.jsx
+++ components/Socials.jsx
@@ -7,12 +7,12 @@ import { TbBrandDrupal } from "react-icons/tb";
 // social icons bar
 export default function Socials() {
   return (
-    <div className="flex text-xl gap-5 items-center">
+    <div className="flex text-xl items-center max-sm:text-2xl">
       <Social url={'https://github.com/alextrandev'} Icon={RiGithubFill} />
       <Social url={'https://github.com/alextrandev'} Icon={RiLinkedinBoxLine} />
       <Social url={'https://github.com/alextrandev'} Icon={TbBrandDrupal} />
       <Social url={'https://github.com/alextrandev'} Icon={RiInstagramLine} />
-      <Social url={'https://github.com/alextrandev'} Icon={LiaBehanceSquare} attr="text-2xl" />
+      <Social url={'https://github.com/alextrandev'} Icon={LiaBehanceSquare} attr="text-2xl max-sm:text-3xl" />
       <Social url={'https://github.com/alextrandev'} Icon={RiDiscordLine} />
       <Social url={'https://github.com/alextrandev'} Icon={LiaSteam} />
     </div>
@@ -22,7 +22,7 @@ export default function Socials() {
 // single social icon link to reuse above
 export function Social({ url, Icon, attr }) {
   return (
-    <Link href={url} target='blank' className={`hover:text-accent transition-all duration-300 ${attr && attr}`}>
+    <Link href={url} target='blank' className={`hover:text-accent transition-all duration-300 p-2 ${attr && attr}`}>
       <Icon />
     </Link>
   )
