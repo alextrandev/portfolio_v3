9700cf44f6df425fc2be0fd8c7fd6d26786f8d5b
Alex Tran <tranduclong092@gmail.com>
2024-07-30 16:25:34 +0300

    Feat: improve styling, add a title for each section

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -115,20 +115,36 @@ export default function About() {
         <Avatar />
       </motion.div>
       <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
-        <div>text</div>
-        <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
-          {aboutData.map((item, itemIndex) =>
-            <div
-              key={`${itemIndex}_${item.title}`}
-              className={
-                `${index == itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} 
+        <div className='flex-1 flex flex-col justify-center'>text</div>
+        <div className='flex flex-col w-full xl:max-w-[48%] h-[480px]'>
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+            {/* section navigation bar */}
+            {aboutData.map((item, itemIndex) =>
+              <a
+                key={`${itemIndex}_${item.title}`}
+                className={
+                  `${index == itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} 
                 cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:bottom-1 after:left-0`
-              }
-              onClick={() => setIndex(itemIndex)}
-            >
-              {item.title}
-            </div>
-          )}
+                }
+                onClick={() => setIndex(itemIndex)}
+              >
+                {item.title}
+              </a>
+            )}
+          </div>
+          <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
+            {/* show current section infos */}
+            {aboutData[index].info.map((item, itemIndex) =>
+              <div
+                key={`${itemIndex}_${item.title}`}
+              >
+                {/* section title */}
+                <p>{item.title}</p>
+                {/* section main */}
+                {/* <div className='hidden md-flex'>-</div> */}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
