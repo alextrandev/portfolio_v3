32cf90cffcdb4e2532b5abdd9a884ffa1a38ae69
Alex Tran <tranduclong092@gmail.com>
2024-08-02 17:09:26 +0300

    Feat: update abut page data and add a hover tooltips

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -54,12 +54,22 @@ export default function About() {
                 <div className='hidden md:flex'>-</div>
                 {/* section text / icons */}
                 <p>{item.year}</p>
-                <div className="flex gap-x-4">
+                <div className="flex">
                   {/* icons */}
-                  {item.icons?.map((icon, itemIndex) =>
-                    <i key={itemIndex} className="text-2xl text-white">
-                      {icon}
-                    </i>
+                  {item.icons?.map(icon =>
+                    <div key={icon.text} className="group px-2 relative">
+                      <div className="text-2xl text-white group-hover:text-accent group-hover:scale-125 transition-all duration-300">
+                        {icon.icon}
+                      </div>
+                      {/* hover tooltip */}
+                      <div className='absolute top-full mt-5 left-1/2 -translate-x-1/2 hidden xl:group-hover:flex z-10'>
+                        <div className='bg-white relative p-1.5 rounded'>
+                          <p className="text-xs font-semibold text-black">{icon.text}</p>
+                          {/* decorative triangle */}
+                          <div className='border-solid border-b-8 border-x-transparent border-x-[6px] border-t-0 absolute -top-2.5 left-1/2  -translate-x-1/2'></div>
+                        </div>
+                      </div>
+                    </div>
                   )}
                 </div>
               </div>
