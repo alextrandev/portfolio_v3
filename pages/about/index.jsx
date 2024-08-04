e1a1351cf2d81ec679211d9e3c447d0f64c70fc4
Alex Tran <tranduclong092@gmail.com>
2024-08-04 14:59:22 +0300

    Feat: create the counter section

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -4,6 +4,7 @@ import Avatar from '../../components/Avatar';
 import { motion } from 'framer-motion';
 import { fadeIn } from '../../variants';
 import { aboutData, statistic } from '../../lib/aboutData';
+import CountUp from 'react-countup';
 
 export default function About() {
   const [index, setIndex] = useState(0);
@@ -25,25 +26,50 @@ export default function About() {
       <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
         {/* about page text blocks */}
         <div className='flex-1 flex flex-col justify-center xl:ml-24'>
-          <h2 className='h2'>
-            <div>How I became a</div>
-            <div><span className='text-accent'>Web developer </span>...</div>
-          </h2>
-          <p className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'>
+          <motion.h2
+            variants={fadeIn('right', 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className='h2 flex flex-wrap justify-center xl:justify-start text-3xl xl:text-5xl'
+          >
+            <span>How I became a</span>&nbsp;
+            <span><span className='text-accent'>Web developer </span>...</span>
+          </motion.h2>
+          <motion.p
+            variants={fadeIn('right', 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'
+          >
             I began working with websites 9 years ago, initially building sites using various CMS platforms.
             After gaining experience in my first job, I decided to dive into full-stack programming, starting with React, Symfony, and Drupal.
-          </p>
-          {/* counter */}
-          <div>
-            <div>
+          </motion.p>
+          {/* counters container */}
+          <motion.div
+            variants={fadeIn('right', 1)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className='hidden md:block mb-3 xl:mb-0'
+          >
+            <div className="flex flex-1 xl:gap-x-6">
+              {/* single counter */}
               {statistic.map((stat, index) =>
-                <div key={`${stat.index}_${stat.text}`} className="">
-                  <div>{stat.number}</div>
-                  <div>{stat.text}</div>
+                <div
+                  key={`${stat.index}_${stat.text}`}
+                  className="relative flex flex-1 flex-col xl:block items-center after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0 last:after:w-0"
+                >
+                  <h3 className='text-lg xl:text-2xl font-extrabold text-accent xl:mb-1'>
+                    <CountUp start={0} end={stat.number} duration={5} />
+                    {stat.plusSign && ' +'}
+                  </h3>
+                  <p className='text-xs tracking-[1px] leading-[1.4] max-w-[100px]'>{stat.text}</p>
                 </div>
               )}
             </div>
-          </div>
+          </motion.div>
         </div>
         {/* info block with multiple sections */}
         <div className='flex flex-col w-full xl:max-w-[48%] h-[330px]'>
@@ -70,7 +96,7 @@ export default function About() {
             )}
           </div>
           {/* show current section infos */}
-          <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
+          <div className="xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
             {aboutData[index].info.map((item, itemIndex) =>
               <div
                 key={`${itemIndex}_${item.title}`}
@@ -78,7 +104,7 @@ export default function About() {
               >
                 {/* section title */}
                 <p className="font-light">{item.title}</p>
-                <p className="font-bold">{item.bold}</p>
+                <p className="font-bold text-accent">{item.bold}</p>
                 {/* next line is a seperation dash, only for bigger devices */}
                 <div className='hidden md:flex'>-</div>
                 {/* section text / icons */}
@@ -106,6 +132,6 @@ export default function About() {
           </div>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
