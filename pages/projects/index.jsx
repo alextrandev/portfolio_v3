babee4c26046f3a566bf2a26626dd7ec12ab7ba3
Alex Tran <tranduclong092@gmail.com>
2024-08-06 21:15:24 +0300

    Feat: add some text and add a slider component to the projects page

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -5,13 +5,21 @@ import ProjectsSlider from './ProjectsSlider';
 export default function projects() {
   return (
     <div className='h-full bg-primary/30 py-36 flex items-center'>
-      <Circles />
-      <div className="container mx-auto">
-        {/* text container */}
-        <div>text</div>
-        {/* projects slider */}
-        <ProjectsSlider />
+      <div className='container mx-auto'>
+        <div className='flex flex-col xl:flex-row gap-x-8'>
+          {/* text container */}
+          <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
+            <h2 className='h2 xl:mt-8'>My projects <span className='text-accent'>.</span></h2>
+            <p className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
+              Check out my recent web projects. Feel free to visit the live websites for a demonstration or checkout the public repository for the source codes
+            </p>
+          </div>
+          {/* projects slider */}
+          <ProjectsSlider />
+        </div>
       </div>
+      {/* 2 decoration components */}
+      <Circles />
       <Bulb />
     </div>
   )
