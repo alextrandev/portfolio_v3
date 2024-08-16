c158402860569688105e47e88b209b2b9c07a9c1
Alex Tran <tranduclong092@gmail.com>
2024-08-16 08:56:15 +0300

    Feat: create a contact form

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -12,7 +12,17 @@ export default function index() {
           </h1>
           {/* contact form */}
           <form className='flex-1 flex items-center flex-col gap-6 w-full mx-auto'>
-            form
+            {/* input group */}
+            <div className="flex gap-x-6 w-full">
+              <input type="text" placeholder="name" className="input" />
+              <input type="text" placeholder="email" className="input" />
+            </div>
+            <input type="text" placeholder="subject" className="input" />
+            <textarea placeholder='message' className='textarea'></textarea>
+            <button className='btn rounded-full border border-white/50 max-w-[170px] px-24 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group'>
+              <span className='group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500'>Send</span>
+              <BsArrowRight className='-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]' />
+            </button>
           </form>
         </div>
       </div>
