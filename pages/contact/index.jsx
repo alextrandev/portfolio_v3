3e148113a8446f3a851273419de6ecf97a21c38b
Alex Tran <tranduclong092@gmail.com>
2024-08-16 08:55:38 +0300

    Feat: create contact page layout

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,5 +1,21 @@
+
+import { BsArrowRight } from 'react-icons/bs';
+
 export default function index() {
   return (
-    <div>index</div>
+    <div className='h-full bg-primary/30'>
+      <div className='container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full'>
+        <div className='flex flex-col w-full max-w-[700px]'>
+          {/* header and text paragraph */}
+          <h1 className='h2 text-center mb-12'>
+            Let's <span className='text-accent'>connect.</span>
+          </h1>
+          {/* contact form */}
+          <form className='flex-1 flex items-center flex-col gap-6 w-full mx-auto'>
+            form
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
