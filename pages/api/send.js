60f40b0238b8014da333f57dc137bc42605f552c
Alex Tran <tranduclong092@gmail.com>
2024-08-26 13:27:37 +0300

    Feat: create an api endpoint for email sending

---
+++ pages/api/send.js
@@ -0,0 +1,20 @@
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export default async (req, res) => {
+  const { name, email, message } = await req.json().then((body) => sendRouteSchema.parse(body));
+
+  const { data, error } = await resend.emails.send({
+    from: 'Sender <sender@alextran.dev>',
+    to: ['reciever@alextran.dev'],
+    subject: `Contact request from ${name} at ${email}`,
+    text: `Message from ${name} at ${email}: ${message}`,
+  });
+
+  if (error) {
+    return res.status(400).json(error);
+  }
+
+  res.status(200).json(data);
+};
