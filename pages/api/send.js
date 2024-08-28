dd3656ffb9beb3f9d429656dd7dd2e81b0567a07
Alex Tran <tranduclong092@gmail.com>
2024-08-28 13:54:33 +0300

    Feat: add form handle functions

--- pages/api/send.js
+++ pages/api/send.js
@@ -3,13 +3,13 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async (req, res) => {
-  const { name, email, message } = await req.json().then((body) => sendRouteSchema.parse(body));
+  const { name, email, subject, message } = await req.json().then((body) => sendRouteSchema.parse(body));
 
   const { data, error } = await resend.emails.send({
     from: 'Sender <sender@alextran.dev>',
     to: ['reciever@alextran.dev'],
     subject: `Contact request from ${name} at ${email}`,
-    text: `Message from ${name} at ${email}: ${message}`,
+    text: `Message from ${name} at ${email}. Subject: ${subject}. Message: ${message}`,
   });
 
   if (error) {
--- pages/contact/Form.jsx
+++ pages/contact/Form.jsx
@@ -1,17 +1,52 @@
+'use client'
+
+import { useState } from "react";
 import FormButton from "./FormButton";
 
 export default function Form() {
+  const [mail, setMail] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(mail);
+  }
+
+  const handleInputChange = (e) => {
+    setMail((prevState) => ({
+      ...prevState,
+      [e.target.placeholder]: e.target.value
+    }))
+  }
+
   return (
-    <form className='flex-1 flex items-center flex-col gap-6 w-full mx-auto'>
+    <form
+      className='flex-1 flex items-center flex-col gap-6 w-full mx-auto'
+      onChange={handleInputChange}
+    >
       {/* input group */}
       <div className="flex gap-x-6 w-full">
-        <input type="text" placeholder="name" className="input" />
-        <input type="text" placeholder="email" className="input" />
+        <input
+          type="text"
+          placeholder="name"
+          className="input"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="input"
+        />
       </div>
-      <input type="text" placeholder="subject" className="input" />
-      <textarea placeholder='message' className='textarea'></textarea>
+      <input
+        type="text"
+        placeholder="subject"
+        className="input"
+      />
+      <textarea
+        placeholder='message'
+        className='textarea'
+      ></textarea>
       {/* submit button */}
-      <FormButton />
+      <FormButton handleSubmit={handleSubmit} />
     </form>
   )
 }
