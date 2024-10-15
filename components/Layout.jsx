aebe9a5fb342e5987b2e1309bd8e14e2f1cd3715
Alex Tran <tranduclong092@gmail.com>
2024-10-15 15:50:58 +0300

    Feat: improve animation speed by refactoring framer motion transition frames

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,6 +2,9 @@ import { Sora } from 'next/font/google';
 import Nav from './Nav';
 import Header from './Header';
 import TopLeftImg from './TopLeftImg';
+import { usePathname } from "next/navigation";
+import Transition from "./Transition";
+import { AnimatePresence } from 'framer-motion';
 
 // font settings
 const sora = Sora({
@@ -11,12 +14,17 @@ const sora = Sora({
 });
 
 export default function Layout({ children }) {
+  const pathname = usePathname();
+
   return (
-    <div className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}>
-      <TopLeftImg />
-      <Nav />
-      <Header />
-      {children}
-    </div>
-  )
+    <AnimatePresence mode="wait">
+      <div key={pathname} className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}>
+        <TopLeftImg />
+        <Nav />
+        <Header />
+        {children}
+        <Transition />
+      </div>
+    </AnimatePresence>
+  );
 }
--- components/Transition.jsx
+++ components/Transition.jsx
@@ -18,35 +18,30 @@ const transitionVariants = {
 export default function Transition() {
   return (
     <>
-      {/* transition frame 1  */}
       <motion.div
-        className="fixed top-0 right-full w-screen h-screen z-30 bg-[#2e2257]"
+        className="fixed top-0 bottom-0 right-full w-screen z-30 bg-[#2e2257]"
         variants={transitionVariants}
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0, duration: 0.7, ease: 'easeOut' }}
-      ></motion.div>
-
-      {/* transition frame 2  */}
+        transition={{ duration: 0.2, ease: "easeInOut" }}
+      />
       <motion.div
-        className="fixed top-0 right-full w-screen h-screen z-20 bg-[#3b2d71]"
+        className="fixed top-0 bottom-0 right-full w-screen z-20 bg-[#3b2d71]"
         variants={transitionVariants}
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.1, duration: 0.7, ease: 'easeOut' }}
-      ></motion.div>
-
-      {/* transition frame 3  */}
+        transition={{ duration: 0.2, delay: 0.1, ease: "easeInOut" }}
+      />
       <motion.div
-        className="fixed top-0 right-full w-screen h-screen z-10 bg-[#4b3792]"
+        className="fixed top-0 bottom-0 right-full w-screen z-10 bg-[#4b3792]"
         variants={transitionVariants}
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.2, duration: 0.7, ease: 'easeOut' }}
-      ></motion.div>
+        transition={{ duration: 0.2, delay: 0.2, ease: "easeInOut" }}
+      />
     </>
-  )
+  );
 }
